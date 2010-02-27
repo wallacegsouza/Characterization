50a9a5e0fc3669c393efe34eaaae6c766348add9
wallace <wallace@MAGI.(none)>
2010-02-27 10:54:00 -0300

    modulos adicionados

---
+++ test.py
@@ -0,0 +1,85 @@
+#! /usr/bin/python
+# coding: utf-8
+
+import unittest
+from models import Path
+from relatorios import GeradorRelatorioTeste
+import os
+
+class test_Path(unittest.TestCase):
+
+    def setUp(self):
+        self.path = Path()
+        self.list_test = [1,2,3,7]
+
+    def test_valor_default(self):
+        p = self.path
+        self.assertEqual(p.color,150)
+        self.assertEqual(p.area,0)
+        self.assertEqual(p.min_horiz,100000)
+        self.assertEqual(p.max_horiz,0)
+        self.assertEqual(p.min_vert,100000)
+        self.assertEqual(p.max_vert,0)
+
+    def config_path(self):
+        p = self.path
+        l = self.list_test
+        for i in l :
+            p.setMinVert(i)
+            p.setMaxVert(i)
+            p.setMaxHoriz(i)
+            p.setMinHoriz(i)
+        return p
+
+    def test_value_max_horizontal(self):
+        p = self.config_path()
+        self.assertEqual(p.max_horiz, 7)
+
+    def test_value_mim_horizontal(self):
+        p = self.config_path()
+        self.assertEqual(p.min_horiz, 1)
+
+    def test_value_max_vertical(self):
+        p = self.config_path()
+        self.assertEqual(p.max_vert, 7)
+
+    def test_value_min_vertical(self):
+        p = self.config_path()
+        self.assertEqual(p.min_vert, 1)
+
+    def test_inc_area(self):
+        p = self.path
+        area_nemos_um = p.area
+        p.incArea()
+        area_mais_um = p.area
+        self.assertTrue(area_nemos_um < area_mais_um)
+
+    def test_comprimento_horizontal(self):
+        p = self.config_path()
+        self.assertEqual(p.comprimentoHorizontal(), 7)
+
+    def test_comprimento_vertical(self):
+        p = self.config_path()
+        self.assertEqual(p.comprimentoVertical(), 7)
+
+    def test_comprimento_medio(self):
+        # (7 + 7) % 2 = 7
+        p = self.config_path()
+        self.assertEqual(p.comprimentoMedio(), 7)
+
+path_name = '/home/wallace/workspace/imagensparatratamento/casoteste'
+
+class test_structure_analysis(unittest.TestCase):
+
+    def setUp(self):
+        self.report = GeradorRelatorioTeste()
+
+    def test_relatorio_intercepto(self):
+        img_name = path_name + '/intercepto.png'
+        self.report.run(img_name=img_name, name='intercepto.png')
+        report_img = open(img_name)
+        
+#        for i in os.listdir(self.path_name):
+#            print i
+
+unittest.main()
---
+++ util.py
@@ -0,0 +1,97 @@
+#! /usr/bin/python
+# coding: utf-8
+
+# threshold 
+t = 12
+
+class Color:
+    """
+    classe para gera um cor qualquer
+    Branco (255,255,255)
+    * Branco - RGB(1,1,1)
+    * Azul - RGB(0,0,1)
+    * Vermelho - RGB(1,0,0)
+    * Verde - RGB(0,1,0)
+    * Amarelo - RGB(1,1,0)
+    * Magenta - RGB(1,0,1)
+    * Ciano - RGB(0,1,1)
+    * Preto - RGB(0,0,0)
+    """
+
+    def __init__(self, color = (0, 0, 0)):
+        self.color = color
+        self.vem_turno = 1
+        self.ver_turno = None
+        self.azu_turno = None
+        self.red_color = (3, 0, 0)
+        self.green_color = (0, 3, 0)
+        self.blue_color = (0, 0, 3)
+        self.maxnumcolor = 0
+
+    def corRGBAliatoria(self, cinza = 0):
+        if cinza < 0 or cinza > 255:
+            return (0, 0, 0)
+
+        if self.vem_turno and self.red_color[0] < 250:
+            self.ver_turno = 1
+            self.vem_turno = None
+            self.color = (self.red_color[0] + t, cinza, cinza)
+            self.red_color = self.color
+
+        else:
+            if self.ver_turno and self.green_color[1] < 250:
+                self.azu_turno = 1
+                self.ver_turno = None
+                self.color = (cinza, self.green_color[1] + t, cinza)
+                self.green_color = self.color
+
+            else:
+                if self.azu_turno and self.blue_color[2] < 250:
+                    self.vem_turno = 1
+                    self.azu_turno = None
+                    self.color = (cinza, cinza, self.blue_color[2] + t)
+                    self.blue_color = self.color
+                else:
+                    return (0, 0, 0)
+
+        self.maxnumcolor = self.maxnumcolor + 1
+        return self.color
+
+    def corHSVAliatoria(self, cinza = 0):
+
+        if cinza < 0 or cinza > 255:
+            return (0, 0, 0)
+
+        if self.vem_turno and self.red_color[0] < 250:
+            self.ver_turno = 1
+            self.vem_turno = None
+            cor = self.red_color[0] + t
+            self.color = (cinza, cor, cor)
+
+        else:
+            if self.ver_turno and self.green_color[1] < 250:
+                self.azu_turno = 1
+                self.ver_turno = None
+                cor = self.green_color[1] + t
+                self.color = (cor, cinza, cor)
+
+            else:
+                if self.azu_turno and self.blue_color[2] < 250:
+                    self.vem_turno = 1
+                    self.azu_turno = None
+                    cor = self.blue_color[2] + t
+                    self.color = (cor, cor, cinza)
+                else:
+                    return (0, 0, 0)
+
+        self.maxnumcolor = self.maxnumcolor + 1
+        return self.color
+
+if __name__ == "__main__":
+    color = Color()
+    for i in range(80):
+            print color.corRGBAliatoria(i)
+            print color.corHSVAliatoria(i)
+
+    print color.corRGBAliatoria(256)
+    print color.maxnumcolor
